9e9bb0c4fc292ad372918c0207e7c83ea7a72c36
G33kDude <GeekDudeAHK@gmail.com>
2021-12-04 22:00:42 -0500

    d4 Made part 2 more readable

--- day4/aoc4.c
+++ day4/aoc4.c
@@ -4,87 +4,139 @@
 #include <mcl.h>
 #include "ahk.h"
 
-BOOL inNums(Object *numbers, int cutoff, int number)
+//
+// Was the number to check called at any point up to the currently called number?
+//
+// Parameters:
+//   numbers             - Array of bingo numbers
+//   called_number_index - The index of the most recently called bingo number in the numbers array
+//   number_to_check     - The number to look for in the called bingo numbers
+//
+BOOL wasCalled(Object *numbers, int called_number_index, int number_to_check)
 {
-	for (int i = 0; i <= cutoff; i++) {
-		if (number == numbers->pFields[i].iValue) {
-			return 1;
+	for (int i = 0; i <= called_number_index; i++) {
+		if (number_to_check == numbers->pFields[i].iValue) {
+			return true;
 		}
 	}
-	return 0;
+	return false;
 }
 
-int invalidSum(Object *board, Object *numbers, int cutoff) {
+//
+// What's the sum of all uncalled numbers on the board?
+//
+// Parameters:
+//   board               - 2D Array of board numbers
+//   numbers             - Array of bingo numbers
+//   called_number_index - The index of the most recently called bingo number in the numbers array
+//
+int uncalledsSum(Object *board, Object *numbers, int called_number_index) {
 	int sum = 0;
-	for (int irow = 0; irow < board->cFields; irow++) {
-		Object* row = board->pFields[irow].pobjValue;
-		for (int icol = 0; icol < row->cFields; icol++) {
-			int val = row->pFields[icol].iValue;
-			if (!inNums(numbers, cutoff, val)) {
+
+	// For each row
+	for (int row_index = 0; row_index < board->cFields; row_index++) {
+		Object* row = board->pFields[row_index].pobjValue;
+
+		// For each column
+		for (int col_index = 0; col_index < row->cFields; col_index++) {
+			int val = row->pFields[col_index].iValue;
+
+			// Add to sum if this cell was never called
+			if (!wasCalled(numbers, called_number_index, val)) {
 				sum += val;
 			}
 		}
 	}
+
 	return sum;
 }
 
-BOOL validBoard(Object *board, Object *numbers, int cutoff) {
-	// Rows
-	for (int irow = 0; irow < board->cFields; irow++) {
-		for (int icol = 0;; icol++) {
-			// Completed row means the board is vaild
-			if (icol >= board->pFields[irow].pobjValue->cFields) {
-				return 1;
+//
+// Does the given board have a row or column with all numbers called?
+//
+// Parameters:
+//   board               - 2D Array of board numbers
+//   numbers             - Array of bingo numbers
+//   called_number_index - The index of the most recently called bingo number in the numbers array
+//
+BOOL boardWins(Object *board, Object *numbers, int called_number_index) {
+
+	// Check all rows
+	for (int row_index = 0; row_index < board->cFields; row_index++) {
+		Object* row = board->pFields[row_index].pobjValue;
+
+		// Look at all columns/cells in the row
+		for (int col_index = 0;; col_index++) {
+
+			// If checking column past end of row, all numbers were called
+			if (col_index >= row->cFields) {
+				return true;
 			}
 
-			// Bail on bad rows
-			if (!inNums(numbers, cutoff, board->pFields[irow].pobjValue->pFields[icol].iValue)) {
+			// Stop checking this row if this cell was not called
+			if (!wasCalled(numbers, called_number_index, row->pFields[col_index].iValue)) {
 				break;
 			}
 		}
 	}
 
+	// Check all columns
+	for (int col_index = 0; col_index < board->pFields[0].pobjValue->cFields; col_index++) {
 
-	// Columns
-	for (int icol = 0; icol < board->pFields[0].pobjValue->cFields; icol++) {
-		for (int irow = 0;; irow++) {
-			// Completed column means the board is vaild
-			if (irow >= board->cFields) {
-				return 1;
+		// Look at all rows/cells in the column
+		for (int row_index = 0;; row_index++) {
+
+			// If checking row past end of column, all numbers were called
+			if (row_index >= board->cFields) {
+				return true;
 			}
 
-			// Bail on bad columns
-			if (!inNums(numbers, cutoff, board->pFields[irow].pobjValue->pFields[icol].iValue)) {
+			// Stop checking this column if this cell was not called
+			Object* row = board->pFields[row_index].pobjValue;
+			if (!wasCalled(numbers, called_number_index, row->pFields[col_index].iValue)) {
 				break;
 			}
 		}
 	}
 
-	return 0;
+	// No full rows or columns detected
+	return false;
 }
 
+MCL_EXPORT(part2);
+TCHAR part2(Object *boards, Object *numbers, Object *board_win_order) {
+	int winning_board_count = 0; // How many boards have won?
+	int winning_called_number_index = -1; // Index of the last winning called number
 
-MCL_EXPORT(part1);
-TCHAR part1(Object *boards, Object *numbers, Object *windex) {
-	int winnercount = 0;
-	int winning_inum = -1;
+	// Call each number in order
+	for (int called_number_index = 0; called_number_index < numbers->cFields; called_number_index++) {
 
-	int last_winner = -1;
-	for (int inum = 0; inum < numbers->cFields; inum++) {
-		for (int iboard = 0; iboard < boards->cFields; iboard++) {
-			Object* board = boards->pFields[iboard].pobjValue;
-			if (validBoard(board, numbers, inum)) {
-				if (windex->pFields[iboard].iValue < 0) {
-					windex->pFields[iboard].iValue = ++winnercount;
-					winning_inum = inum;
-				}
+		// For all boards
+		for (int board_index = 0; board_index < boards->cFields; board_index++) {
+			Object* board = boards->pFields[board_index].pobjValue;
+
+			// If the board is in a win state and didn't already win
+			if (boardWins(board, numbers, called_number_index)
+				&& board_win_order->pFields[board_index].iValue < 0) {
+
+				// Mark its place in the winner's order
+				board_win_order->pFields[board_index].iValue = ++winning_board_count;
+
+				// Keep track of the index of the last winning number index
+				winning_called_number_index = called_number_index;
 			}
 		}
 	}
-	
-	for (int iboard = 0; iboard < boards->cFields; iboard++) {
-		if (windex->pFields[iboard].iValue == winnercount) {
-			return numbers->pFields[winning_inum].iValue * invalidSum(boards->pFields[iboard].pobjValue, numbers, winning_inum);
+
+	// Find the board that won last
+	for (int board_index = 0; board_index < boards->cFields; board_index++) {
+		if (board_win_order->pFields[board_index].iValue == winning_board_count) {
+			Object* board = boards->pFields[board_index].pobjValue;
+
+			// Calculate and return the puzzle solution
+			int winning_number = numbers->pFields[winning_called_number_index].iValue;
+			int uncalleds_sum = uncalledsSum(board, numbers, winning_called_number_index);
+			return winning_number * uncalleds_sum;
 		}
 	}
 
--- day4/aoc4_2.ahk
+++ day4/aoc4_2.ahk
@@ -8,21 +8,37 @@ MCL.CompilerSuffix := ""
 
 lib := MCL.FromC(FileOpen(A_ScriptDir "\aoc4.c", "r").Read())
 
-obj := StrSplit(RegExReplace(RTrim(FileOpen(A_ScriptDir "\input4.txt", "r").Read(), "`r`n"), "`a)\R", "`n"), "`n`n")
+input := FileOpen(A_ScriptDir "\input4.txt", "r").Read()
+input := RTrim(input, "`r`n") ; Remove trailing newlines
+input := RegExReplace(input, "`a)\R", "`n") ; Normalize newlines to single linefeed
+input := StrSplit(input, "`n`n") ; Split input on double newline
 
-numbers := StrSplit(obj.RemoveAt(1), ",")
-for k, v in numbers
-	numbers[k] := v+0
+numbers := []
 boards := []
+board_win_order := []
 
-windex := []
-for iboard, board in obj {
-	windex[iboard] := -1
-	for irow, row in StrSplit(board, "`n", "`r") {
-		for icol, col in StrSplit(RegExReplace(Trim(row, " "), "\s+", " "), " ")
-			boards[iboard, irow, icol] := col+0
+; Grab and numerify bingo numbers
+for k, v in StrSplit(input.RemoveAt(1), ",")
+	numbers[k] := v+0
+
+; Grab and process bingo boards
+for board_index, board in input
+{
+	; Mark board win order as -1 for all at start
+	board_win_order[board_index] := -1
+
+	; Split up each board
+	for row_index, row in StrSplit(board, "`n", "`r")
+	{
+		row := Trim(row, " ") ; Strip leading spaces
+		row := RegExReplace(row, "\s+", " ") ; Collapse multi-space to single space
+		row := StrSplit(row, " ") ; Split on space
+
+		; Numerify and set cell value
+		for col_index, col in row
+			boards[board_index, row_index, col_index] := col+0
 	}
 }
 
-MsgBox, % DllCall(lib.part1, "Ptr", &boards, "Ptr", &numbers, "Ptr", &windex, "CDecl Int")
+MsgBox, % DllCall(lib.part2, "Ptr", &boards, "Ptr", &numbers, "Ptr", &board_win_order, "CDecl Int")
 
